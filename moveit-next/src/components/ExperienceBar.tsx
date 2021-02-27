831f3f2db65d30b2e869bc02e8c7dd93986bc7cf
felipegenef <felipegenef@gmail.com>
2021-02-27 17:10:17 -0300

    fix

--- moveit-next/src/components/ExperienceBar.tsx
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -3,7 +3,7 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export default function ExperienceBar() {
-    const {currentExperience,experienceToNextLevel}=useContext(ChallengesContext);
+    const {currentExperience,experienceToNextLevel}=useContext(ChallengesContext); 
     const percentToNextLevel=Math.round(currentExperience*100)/experienceToNextLevel
     return (
        <header className={styles.experienceBar}>
